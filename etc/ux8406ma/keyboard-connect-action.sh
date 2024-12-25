593e450e64c3a348815d282dc70b4ac470d1e7ef
lcoronelp <lcoronel@leocoronel.com>
2024-12-25 16:48:31 +0000

    try to keep the brightness

--- etc/ux8406ma/keyboard-connect-action.sh
+++ etc/ux8406ma/keyboard-connect-action.sh
@@ -3,8 +3,13 @@
 # Stop the rotation monitor service
 systemctl stop ux8406ma-screen-rotation-monitor
 
+# Get current brightness of eDP-1
+EDP1_CURRENT_BRIGHTNESS=$(DISPLAY=:0 xrandr --verbose | grep -A 5 "^eDP-1" | grep "Brightness" | awk '{print $2}')
+
+
 # Turn Off eDP-2 and rotate eDP-1 to normal
 DISPLAY=:0 xrandr --output eDP-1 --rotate normal --output eDP-2 --off
+DISPLAY=:0 xrandr --output eDP-1 --brightness $EDP1_CURRENT_BRIGHTNESS
 
 # Log
 /etc/ux8406ma/log-manager.sh "Keyboard connected. Turn eDP-1 restored to normal, turn Off eDP-2 and stop screen rotation monitor service."
--- etc/ux8406ma/keyboard-disconnect-action.sh
+++ etc/ux8406ma/keyboard-disconnect-action.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# Get current brightness of eDP-1
+EDP1_CURRENT_BRIGHTNESS=$(DISPLAY=:0 xrandr --verbose | grep -A 5 "^eDP-1" | grep "Brightness" | awk '{print $2}')
+
 # Turn on eDP-2 and position it below eDP-1
 DISPLAY=:0 xrandr --output eDP-2 --auto --below eDP-1
+DISPLAY=:0 xrandr --output eDP-1 --brightness $EDP1_CURRENT_BRIGHTNESS
 
 # Start the rotation monitor service
 systemctl start ux8406ma-screen-rotation-monitor
